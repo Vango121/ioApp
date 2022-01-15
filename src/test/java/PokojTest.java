2cf06d2ff55abafe41e216bff945f102b41765fe
Vango121 <barski93@gmail.com>
2022-01-15 13:17:49 +0100

    add testSprawdzRezerwacje

--- src/test/java/PokojTest.java
+++ src/test/java/PokojTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class PokojTest {
@@ -17,10 +18,10 @@ public class PokojTest {
 
     @BeforeAll
     void init() {
-    instances = new Pokoj[2];
-    instances[0] = new Pokoj(0,50,true);
-    instances[1] = new Pokoj(1,30,false);
-    dane = new Dane();
+        instances = new Pokoj[2];
+        dane = new Dane();
+        instances[0] = dane.pokoje[0];
+        instances[1] = dane.pokoje[1];
     }
 
     private static Stream<Arguments> addFixture() {
@@ -30,14 +31,35 @@ public class PokojTest {
 
     @ParameterizedTest
     @MethodSource("addFixture")
-    void testDodajRezerwacje(Rezerwacja[] rezerwacje){
-        for (int i = 0; i <2; i++) {
-            for (int j = 0; j <4; j++) {
+    void testDodajRezerwacje(Rezerwacja[] rezerwacje) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
                 instances[i].dodajRezerwacje(rezerwacje[j]);
                 Rezerwacja rezerwacja1 = instances[i].getRezerwacje().get(j);
-                assertSame(rezerwacja1,rezerwacje[j]);
+                assertSame(rezerwacja1, rezerwacje[j]);
+            }
+        }
+        assertEquals(4, instances[0].getRezerwacje().size());
+        assertEquals(4, instances[1].getRezerwacje().size());
+    }
+
+    @ParameterizedTest
+    @MethodSource("addFixture")
+    void testSprawdzRezerwacje(Rezerwacja[] rezerwacje) {
+        //instances[0] - pokoj[0] sprawdz
+        for (int i = 0; i <4; i++) {
+            if(i<2){
+                assertEquals(instances[0],rezerwacje[i].getPokoj());
+            }else{
+                assertNotEquals(instances[0],rezerwacje[i].getPokoj());
+            }
+        }
+        for (int i = 0; i <4; i++) {
+            if(i<2){
+                assertNotEquals(instances[1],rezerwacje[i].getPokoj());
+            }else{
+                assertEquals(instances[1],rezerwacje[i].getPokoj());
             }
         }
     }
-
 }
